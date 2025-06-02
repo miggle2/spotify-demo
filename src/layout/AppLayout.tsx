5f410961b8092bc6c300abdd1762dd05d4503182
Mikyung Cho <jmk2633@gmail.com>
2025-06-03 00:29:15 +0900

    url 확인하기 편하게

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { Outlet } from 'react-router'
+import { useNavigate } from 'react-router'
 
 const AppLayout = () => {
+    
+    const navigate = useNavigate();
+
   return (
     <div>
         Sidebar
+        <div>
+        <button onClick={()=> navigate('/')}>Home</button>
+        <button onClick={()=> navigate('/search')}>Search</button>
+        <button onClick={()=> navigate('/search/{keyword}')}>SearchWithKeyword</button>
+        <button onClick={()=> navigate('/playlist/{1}')}>PlaylistDetailPage</button>
+        </div>
         <Outlet/>
     </div>
     
