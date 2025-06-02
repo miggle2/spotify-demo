4b5ce50fb7df8d65cbf9db7817eec33607bbe98b
Mikyung Cho <jmk2633@gmail.com>
2025-06-02 23:54:32 +0900

    import haha

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === "production";
