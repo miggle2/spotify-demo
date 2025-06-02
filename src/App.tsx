0f76a8e3fa2d5268d92028ede554b085d75c5bb5
Mikyung Cho <jmk2633@gmail.com>
2025-06-02 23:20:22 +0900

    웹팩,바벨,라우터

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router';
+import AppLayout from './layout/AppLayout';
+import HomePage from './pages/HomePage/HomePage';
+import SearchPage from './pages/SearchPage/SearchPage';
+import SearchWithKeywordPage from './pages/SearchWithKeywordPage/SearchWithKeywordPage';
+import PlaylistDetailPage from './pages/PlaylistDetailPage/PlaylistDetailPage';
+import PlaylistPage from './pages/PlaylistPage/PlaylistPage';
+
+// 0. 사이드바 있어야함 (플레이리스트, 메뉴)
+// 1. 홈페이지  /
+// 2. 서치페이지  /search
+// 3. 서치 결과 페이지  /search/:keyword
+// 4. 플레이리스트 디테일 페이지  /playlist/:id
+// 5. (모바일버전) 플레이리스트 보여주는 페이지 /playlist
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<AppLayout/>}>
+        <Route index element={<HomePage/>}/>
+        <Route path="search" element={<SearchPage/>}/>
+        <Route path="search/:keyword" element={<SearchWithKeywordPage/>}/>
+        <Route path="playlist/:id" element={<PlaylistDetailPage/>}/>
+        {/* <Route path="playlist" element={<PlaylistPage/>}/> */}
+      </Route>
+    </Routes>
   );
 }
 
