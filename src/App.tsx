5f2835ff1a201dc3cae9bc78c4e9be36d7c31555
Mikyung Cho <jmk2633@gmail.com>
2025-06-06 13:27:14 +0900

    로그인

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React, {Suspense, lazy} from 'react';
+import React, { Suspense, lazy, useEffect } from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router';
 import AppLayout from './layout/AppLayout';
 import LoadingSpinner from './common/components/LoadingSpinner';
+import useExchangeToken from './hooks/useExchangeToken';
 
 //lazy import
-const HomePage = lazy(()=> import('./pages/HomePage/HomePage'));
-const SearchPage = lazy(()=> import('./pages/SearchPage/SearchPage'));
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const SearchPage = lazy(() => import('./pages/SearchPage/SearchPage'));
 const SearchWithKeywordPage = lazy(() => import('./pages/SearchWithKeywordPage/SearchWithKeywordPage'));
 const PlaylistDetailPage = lazy(() => import('./pages/PlaylistDetailPage/PlaylistDetailPage'));
 //const PlaylistPage = lazy(() => import('./pages/PlaylistPage/PlaylistPage'));
@@ -20,14 +21,25 @@ const PlaylistDetailPage = lazy(() => import('./pages/PlaylistDetailPage/Playlis
 // 5. (모바일버전) 플레이리스트 보여주는 페이지 /playlist
 
 function App() {
+  const urlParams = new URLSearchParams(window.location.search);
+  let code = urlParams.get('code');
+  let codeVerifier = localStorage.getItem('code_verifier')
+  const { mutate: exchangeToken } = useExchangeToken()
+
+  useEffect(() => {
+    if (code && codeVerifier) {
+      exchangeToken({ code, codeVerifier });
+    }
+  }, [code, codeVerifier, exchangeToken])
+
   return (
-    <Suspense fallback={<LoadingSpinner/>}>
+    <Suspense fallback={<LoadingSpinner />}>
       <Routes>
-        <Route path="/" element={<AppLayout/>}>
-          <Route index element={<HomePage/>}/>
-          <Route path="search" element={<SearchPage/>}/>
-          <Route path="search/:keyword" element={<SearchWithKeywordPage/>}/>
-          <Route path="playlist/:id" element={<PlaylistDetailPage/>}/>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="search" element={<SearchPage />} />
+          <Route path="search/:keyword" element={<SearchWithKeywordPage />} />
+          <Route path="playlist/:id" element={<PlaylistDetailPage />} />
           {/* <Route path="playlist" element={<PlaylistPage/>}/> */}
         </Route>
       </Routes>
